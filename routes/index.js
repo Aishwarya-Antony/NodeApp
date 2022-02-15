9f0bd6d318752eda09ae872bca83a5810f0f7d34
Aishwarya-Antony <83328041+Aishwarya-Antony@users.noreply.github.com>
2022-02-15 17:08:09 +0530

    d

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  console.log("****************************")
+  // students.push({name: "Bala"})
+  res.json({"name": "Bala"});
 });
 
 module.exports = router;
